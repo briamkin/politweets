f2a2f3b4527004fe89ddf95c3717e1c08b01fddb
briamkin <briam.kin@gmail.com>
2015-05-29 15:00:49 -0400

    new geo files

--- geo.py
+++ geo.py
@@ -2,25 +2,22 @@ import matplotlib.path as mplPath
 import numpy as np
 
 def in_polygon(point, poly):
-  # bbPath = mplPath.Path(np.array(points))
-  # return bbPath.contains_point(point)
   x = point[0]
   y = point[1]
   n = len(poly)
-  inside = False
+  status = False
   p1x,p1y = poly[0]
   for i in range(n+1):
-      p2x,p2y = poly[i % n]
-      if y > min(p1y,p2y):
-          if y <= max(p1y,p2y):
-              if x <= max(p1x,p2x):
-                  if p1y != p2y:
-                      xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                  if p1x == p2x or x <= xinters:
-                      inside = not inside
-      p1x,p1y = p2x,p2y
+    p2x,p2y = poly[i % n]
+    if min(p1y,p2y) < y <= max(p1y,p2y):
+      if x <= max(p1x,p2x):
+        if p1y != p2y:
+          xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+        if p1x == p2x or x <= xinters:
+          status = not status
+    p1x,p1y = p2x,p2y
 
-  return inside
+  return status
 
 def rect(point, points):
   # define points from
