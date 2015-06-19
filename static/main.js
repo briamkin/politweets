3b58aa260e7d0508da31903c177f16a5dfbabcb3
briamkin <briam.kin@gmail.com>
2015-06-19 02:14:26 -0400

    almost there...

--- static/main.js
+++ static/main.js
@@ -1,6 +1,6 @@
 // Create AJAX loader
-(function(){
-  var spin_opts = { lines: 13, length: 13, width: 3, radius: 15, corners: 0, rotate: 0, direction: 1, color: '#000', speed: 1, trail: 42, shadow: false, hwaccel: false, className: 'ajax-spinner', zIndex: 2e9, top: '50%', left: '50%', color: '#2171b5' };
-  var target = document.getElementById('ajax-loader');
-  var spinner = new Spinner(spin_opts).spin(target);
-}());
+// (function(){
+//   var spin_opts = { lines: 13, length: 13, width: 3, radius: 15, corners: 0, rotate: 0, direction: 1, color: '#000', speed: 1, trail: 42, shadow: false, hwaccel: false, className: 'ajax-spinner', zIndex: 2e9, top: '50%', left: '50%', color: '#2171b5' };
+//   var target = document.getElementById('ajax-loader');
+//   var spinner = new Spinner(spin_opts).spin(target);
+// }());
